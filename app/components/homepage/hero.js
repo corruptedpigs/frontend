43b8567e25f3b7438eb18c4cb43b8e2a55eb13f8
Nuno Costa <nuno.mmc@gmail.com>
2024-05-07 17:30:15 +0100

    Add 2nd section to homepage

--- app/components/homepage/hero.js
+++ app/components/homepage/hero.js
@@ -1,52 +1,80 @@
 // import InstituitionsRollbar from './institutions_rollbar';
-import Link from "next/link";
-import { getImageProps } from 'next/image';
-import Navbar from "./navbar"
+"use client";
 
-function getBackgroundImage(srcSet = '') {
-  const imageSet = srcSet
-    .split(', ')
-    .map((str) => {
-      const [url, dpi] = str.split(' ')
-      return `url("${url}") ${dpi}`
-    })
-    .join(', ')
-  return `image-set(${imageSet})`
+import { motion } from 'framer-motion';
+import Image, { getImageProps } from 'next/image';
+import Navbar from "./navbar"
+import React, { useState } from 'react';
+
+
+
+function getBackgroundImage(sectionName, imageData) {
+  if (sectionName in imageData) {
+    return `url('${imageData[sectionName]}')`;  // Format background image URL
+  } else {
+    throw new Error(`Background image not found for section: ${sectionName}`);
+  }
 }
 
 const Hero = () => {
-  const {
-    props: { srcSet },
-  } = getImageProps({ alt: '', width: 1264, height: 656, src: '/background.jpeg' })
-  const backgroundImage = getBackgroundImage(srcSet)
+  const imageData = {
+    hero: '/background.jpeg',
+    footer: '/background-2.jpeg',
+  };
 
-  const style = {
-    height: '100vh',
-    width: '100vw',
-    backgroundImage,
-    backgroundSize: 'cover',
-    position: 'relative', // Needed for overlay positioning
-  }
+  // const {
+  //   props: { srcSet },
+  // } = getImageProps({ alt: '', width: 900, height: 683, src: '/background.jpeg' })
+  const heroBackgroundImage = getBackgroundImage('hero', imageData);
+  const hero2BackgroundImage = getBackgroundImage('footer', imageData);
 
   return (
-    <div style={{ backgroundImage: backgroundImage, backgroundSize: "cover" }}>
-      <Navbar />
+    <>
+      <section id="hero1" style={{ backgroundImage: heroBackgroundImage, backgroundSize: "cover" }}>
+        <Navbar />
 
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content grid grid-cols-1 text-neutral-content">
-        <div className="flex justify-between">  <div>
-        <h1 className="mb-5 text-5xl font-bold">Hero Label 1</h1>
-        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-warning uppercase">Launch App</button>
-      </div>
-      <div>
-        <img src="/carta1.png" alt="Image description" class="rounded" />
-        {/* <img src="/carta2.png" alt="Image description" class="rounded" />
-        <img src="/carta3.png" alt="Image description" class="rounded" /> */}
-      </div>
-    </div>
-  </div>
-</div>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content grid grid-cols-1 text-neutral-content container mx-auto py-20">
+          <div className="flex justify-between">
+            <div>
+              <h1 className="mb-5 text-5xl font-bold">Hero Label 1</h1>
+              <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+              <button class="btn btn-warning uppercase">Launch App</button>
+            </div>
+            <motion.div
+              initial={{ rotateY: 0 }}
+              animate={{ rotateY: 360 }} // One full rotation
+              duration={2} // Adjust duration (in seconds)
+              ease="easeInOut" // Adjust easing function
+              style={{ display: 'flex', /* additional styles */ }}
+            >
+              {/* Your image elements here */}
+              <img src="/carta1.png" alt="Image description" />
+            </motion.div>
+          </div>
+        </div>
+      </section>
+
+      <section id="hero2" style={{ backgroundImage: hero2BackgroundImage, backgroundSize: "cover" }} className='hero'>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content grid grid-cols-1 container py-20">
+          <div className="flex justify-between flex-row-reverse">
+            <div className='text-neutral-content mx-10'>
+              <h1 className="mb-5 text-5xl font-bold">Hero Label 2</h1>
+              <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            </div>
+            <div className="mockup-browser border bg-slate-300">
+              <div className="mockup-browser-toolbar">
+                <div className="input">https://corruptedpigs.com</div>
+              </div>
+              <div className="flex justify-center px-4 py-6 bg-slate-800">
+                <Image src="/game-demo.jpeg" height={566} width={900}></Image>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- app/components/homepage/navbar.js
+++ app/components/homepage/navbar.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <>
+    <div className="container mx-auto">
       <div className="navbar text-neutral-content">
         <div className="flex-1">
           <Image src="/logo.png"
@@ -19,13 +19,14 @@ const Navbar = () => {
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1 uppercase font-semibold">
-            <li><Link href="//corruptedpigs.store">Merchandising</Link></li>
             <li><Link href="#">Games</Link></li>
-            <li><Link href="#" className="btn btn-sm btn-warning btn-disabled">Launch App</Link></li>
+            <li><Link href="//corruptedpigs.store">Merchandising</Link></li>
+            <li><Link href="#">About</Link></li>
+            <li><Link href="#" className="btn btn-sm btn-warning">Launch App</Link></li>
           </ul>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
