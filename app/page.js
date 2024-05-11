5ed652113704d9d30ad47c0dd7ceb0c06bd0264d
Nuno Costa <nuno.mmc@gmail.com>
2024-05-11 13:10:50 +0100

    Add Google Analytics

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from 'react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import Hero from "./components/homepage/hero"
 import NFCBanner from "./components/homepage/nfc_banner"
 import Footer from "./components/homepage/footer"
 
 export default function Home() {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      // Initialize Google Analytics only in production
+      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS} />
+    }
+  }, []);
+
   return (
     <main className="min-h-screen">
       <Hero/>
