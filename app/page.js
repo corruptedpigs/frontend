7ce00303eef338ba2e9a0b9798078c76c4afe2af
Nuno Costa <nuno.mmc@gmail.com>
2024-05-11 14:14:16 +0100

    moving GA- code to layout

--- app/page.js
+++ app/page.js
@@ -1,17 +1,13 @@
 "use client";
 
 import { useEffect } from 'react';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import Hero from "./components/homepage/hero"
 import NFCBanner from "./components/homepage/nfc_banner"
 import Footer from "./components/homepage/footer"
 
 export default function Home() {
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      // Initialize Google Analytics only in production
-      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS} />
-    }
+    console.error("The NODE_ENV is " + process.env.NODE_ENV);
   }, []);
 
   return (
