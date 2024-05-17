22c178f7c89ffd4d43bc408939e2c2072e79366b
Nuno Costa <nuno.mmc@gmail.com>
2024-05-17 23:38:40 +0100

    Appease warning The "images.domains" configuration is deprecated.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['assets-global.website-files.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets-global.website-files.com',
+      },
+    ],
   },
 };
 
