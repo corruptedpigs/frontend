8bf1d01e21648a1caadde55c36affaa534ada457
Nuno Costa <nuno.mmc@gmail.com>
2024-05-19 00:20:37 +0100

    put pig on top of roadmap timeline

--- app/components/homepage/roadmap.js
+++ app/components/homepage/roadmap.js
@@ -1,114 +1,127 @@
+import Image from "next/image";
+
 const Roadmap = () => {
   return (
     <section id="roadmap" className="bg-violet py-10 md:py-16">
-      <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold mb-4 text-center text-neutral-content">Roadmap</h2>
-        <ul className="timeline timeline-vertical mx-auto max-w-screen-lg">
+      <div className="container mx-auto px-4 text-neutral-content">
+        <h2 className="text-4xl font-bold mb-8 text-center">Roadmap</h2>
+        <ul className="timeline timeline-snap-icon timeline-vertical mx-auto max-w-screen-lg">
           <li>
-            <div className="timeline-end text-neutral-content">Q1 2024</div>
+            <div className="timeline-end">Q1 2024</div>
             <div className="timeline-middle">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+              <Image src="/logo.png"
+                alt="corrupted pigs logo"
+                className="mb-1"
+                height={45}
+                width={45}
+                priority
+              />
             </div>
-            <div className="timeline-start timeline-box">
-              <p>
-                <span className="font-bold">Project Conceptualization:</span> Define on which blockchain the NFTs will be deployed.
-              </p>
-              <p>
-              <span className="font-bold">Team Structure:</span> Recruit key team members, including developers, designers, and community managers.
-              </p>
+            <div className="timeline-start timeline-box bg-transparent">
+              <dl>
+                <dt>Project Conceptualization</dt>
+                <dd>Define on which blockchain the NFTs will be deployed</dd>
+
+                <dt>Team Structure</dt>
+                <dd>Recruit key team members, including developers, designers, and community managers.</dd>
+              </dl>
             </div>
-            <hr className="bg-purple"/>
+            <hr className="bg-red-700"/>
           </li>
           <li>
-            <hr className="bg-purple"/>
-            <div className="timeline-start text-neutral-content">Q2 2024</div>
+            <hr className="bg-orange-700"/>
+            <div className="timeline-start">Q2 2024</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end timeline-box">
-              <p>
-                <span className="font-bold">Project Conceptualization:</span> Finalize game mechanics and NFT design.
-              </p>
-              <p>
-              <span className="font-bold">Smart Contract Development:</span> Develop and test smart contracts for NFTs and in-game transactions.
-              </p>
-              <p>
-              <span className="font-bold">Community Building:</span> Launch social media channels and engage early adopters.
-              </p>
+            <div className="timeline-end timeline-box bg-transparent">
+              <dl>
+                <dt>Project Conceptualization</dt>
+                <dd>Finalize game mechanics and NFT design.</dd>
+
+                <dt>Smart Contract Development</dt>
+                <dd>Develop and test smart contracts for NFTs and in-game transactions.</dd>
+
+                <dt>Community Building</dt>
+                <dd>Launch social media channels and engage early adopters.</dd>
+              </dl>
             </div>
-            <hr className="bg-purple"/>
+            <hr className="bg-orange-600"/>
           </li>
           <li>
-            <hr className="bg-purple"/>
-            <div className="timeline-start timeline-box">
-              <p>
-                <span className="font-bold">NFT Collection Launch:</span> Release the first batch of Corrupted Pigs NFTs on the Polygon network.
-              </p>
-              <p>
-                <span className="font-bold">Marketplace Integration:</span> List NFTs on major NFT marketplaces for trading.
-              </p>
-              <p>
-                <span className="font-bold">Beta Game Launch:</span> Release a beta version of the burning pigs game for selected players.
-              </p>
+            <hr className="bg-yellow-300"/>
+            <div className="timeline-start timeline-box bg-transparent">
+              <dl>
+                <dt>NFT Collection Launch</dt>
+                <dd>Release the first batch of Corrupted Pigs NFTs on the Polygon network.</dd>
+
+                <dt>Marketplace Integration</dt>
+                <dd>List NFTs on major NFT marketplaces for trading.</dd>
+
+                <dt>Beta Game Launch</dt>
+                <dd>Release a beta version of the burning pigs game for selected players.</dd>
+              </dl>
             </div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end text-neutral-content">Q3 2024</div>
+            <div className="timeline-end">Q3 2024</div>
             <hr/>
           </li>
           <li>
             <hr/>
-            <div className="timeline-start text-neutral-content">Q4 2024</div>
+            <div className="timeline-start">Q4 2024</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end timeline-box">
-              <p>
-                <span className="font-bold">Full Game Launch:</span> Officially launch the Corrupted Pigs social game with full functionality.
-              </p>
-              <p>
-                <span className="font-bold">Community Events:</span> Host events and contests to boost engagement and reward participants
-              </p>
-            </div>
-            <hr/>
-          </li>
-          <li>
-            <hr/>
-            <div className="timeline-end text-neutral-content">Q1 2025</div>
-            <div className="timeline-middle">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
-            </div>
-            <div className="timeline-start timeline-box">
-              <p>
-                <span className="font-bold">Exclusive Content:</span> Release special NFTs and in-game items for top players and contributors.
-              </p>
-              <p>
-                <span className="font-bold">Governance:</span> Implement a community governance system for player input on future developments.
-              </p>
-              <p>
-                <span className="font-bold">Partnerships:</span> Form strategic partnerships with other blockchain projects and organizations.
-              </p>
+            <div className="timeline-end timeline-box bg-transparent">
+              <dl>
+                <dt>Full Game Launch</dt>
+                <dd>Officially launch the Corrupted Pigs social game with full functionality.</dd>
+
+                <dt>Community Events</dt>
+                <dd>Host events and contests to boost engagement and reward participants.</dd>
+              </dl>
             </div>
             <hr/>
           </li>
           <li>
             <hr/>
-            <div className="timeline-start text-neutral-content">Q2 2025</div>
+            <div className="timeline-end">Q1 2025</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end timeline-box">
-            <p>
-                <span className="font-bold">Continuous Updates:</span> Regularly update the game with new features, content, and improvements.
-              </p>
-              <p>
-                <span className="font-bold">Expansion:</span> Explore opportunities for cross-game interoperability and additional blockchain integrations.
-              </p>
-              <p>
-                <span className="font-bold">Sustainability Initiatives:</span> Launch programs to support social and environmental causes through in-game activities.
-.
-              </p>
+            <div className="timeline-start timeline-box bg-transparent">
+              <dl>
+                <dt>Exclusive Content</dt>
+                <dd>Release special NFTs and in-game items for top players and contributors.</dd>
+
+                <dt>Governance</dt>
+                <dd>Implement a community governance system for player input on future developments.</dd>
+
+                <dt>Partnerships</dt>
+                <dd>Form strategic partnerships with other blockchain projects and organizations.</dd>
+              </dl>
+            </div>
+            <hr/>
+          </li>
+          <li>
+            <hr/>
+            <div className="timeline-start">Q2 2025</div>
+            <div className="timeline-middle">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+            </div>
+            <div className="timeline-end timeline-box bg-transparent">
+              <dl>
+                <dt>Continuous Updates</dt>
+                <dd>Regularly update the game with new features, content, and improvements.</dd>
+
+                <dt>Expansion</dt>
+                <dd>Explore opportunities for cross-game interoperability and additional blockchain integrations.</dd>
+
+                <dt>Sustainability Initiatives</dt>
+                <dd>Launch programs to support social and environmental causes through in-game activities.</dd>
+              </dl>
             </div>
           </li>
         </ul>
