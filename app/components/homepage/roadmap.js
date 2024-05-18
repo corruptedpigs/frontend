fdbc5436664f7728c3f23c4fe2538e59a54e86a4
Nuno Costa <nuno.mmc@gmail.com>
2024-05-18 23:41:31 +0100

    Revert "Update roadmap"
    This reverts commit 2e44555828363d1da6fde7886c910cfe9bb84430.

--- app/components/homepage/roadmap.js
+++ app/components/homepage/roadmap.js
@@ -5,11 +5,11 @@ const Roadmap = () => {
         <h2 className="text-4xl font-bold mb-4 text-center text-neutral-content">Roadmap</h2>
         <ul className="timeline timeline-vertical mx-auto max-w-screen-lg">
           <li>
-            <div className="timeline-end timeline-box">Q1 2024</div>
+            <div className="timeline-end text-neutral-content">Q1 2024</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-start text-neutral-content">
+            <div className="timeline-start timeline-box">
               <p>
                 <span className="font-bold">Project Conceptualization:</span> Define on which blockchain the NFTs will be deployed.
               </p>
@@ -21,11 +21,11 @@ const Roadmap = () => {
           </li>
           <li>
             <hr className="bg-purple"/>
-            <div className="timeline-start timeline-box">Q2 2024</div>
+            <div className="timeline-start text-neutral-content">Q2 2024</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end text-neutral-content">
+            <div className="timeline-end timeline-box">
               <p>
                 <span className="font-bold">Project Conceptualization:</span> Finalize game mechanics and NFT design.
               </p>
@@ -40,7 +40,7 @@ const Roadmap = () => {
           </li>
           <li>
             <hr className="bg-purple"/>
-            <div className="timeline-start text-neutral-content">
+            <div className="timeline-start timeline-box">
               <p>
                 <span className="font-bold">NFT Collection Launch:</span> Release the first batch of Corrupted Pigs NFTs on the Polygon network.
               </p>
@@ -54,16 +54,16 @@ const Roadmap = () => {
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end timeline-box">Q3 2024</div>
+            <div className="timeline-end text-neutral-content">Q3 2024</div>
             <hr/>
           </li>
           <li>
             <hr/>
-            <div className="timeline-start timeline-box">Q4 2024</div>
+            <div className="timeline-start text-neutral-content">Q4 2024</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end text-neutral-content">
+            <div className="timeline-end timeline-box">
               <p>
                 <span className="font-bold">Full Game Launch:</span> Officially launch the Corrupted Pigs social game with full functionality.
               </p>
@@ -75,11 +75,11 @@ const Roadmap = () => {
           </li>
           <li>
             <hr/>
-            <div className="timeline-end timeline-box">Q1 2025</div>
+            <div className="timeline-end text-neutral-content">Q1 2025</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-start text-neutral-content">
+            <div className="timeline-start timeline-box">
               <p>
                 <span className="font-bold">Exclusive Content:</span> Release special NFTs and in-game items for top players and contributors.
               </p>
@@ -94,11 +94,11 @@ const Roadmap = () => {
           </li>
           <li>
             <hr/>
-            <div className="timeline-start timeline-box">Q2 2025</div>
+            <div className="timeline-start text-neutral-content">Q2 2025</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end text-neutral-content">
+            <div className="timeline-end timeline-box">
             <p>
                 <span className="font-bold">Continuous Updates:</span> Regularly update the game with new features, content, and improvements.
               </p>
