0c4c01af3244f6ea837c3987326aecb2393afe27
Nuno Costa <nuno.mmc@gmail.com>
2024-05-19 01:06:46 +0100

    glow timeline

--- app/components/homepage/roadmap.js
+++ app/components/homepage/roadmap.js
@@ -5,15 +5,15 @@ const Roadmap = () => {
     <section id="roadmap" className="bg-violet py-10 md:py-16">
       <div className="container mx-auto px-4 text-neutral-content">
         <h2 className="text-4xl font-bold mb-8 text-center">Roadmap</h2>
-        <ul className="timeline timeline-snap-icon timeline-vertical mx-auto max-w-screen-lg">
+        <ul className="timeline timeline-vertical mx-auto max-w-screen-lg">
           <li>
-            <div className="timeline-end">Q1 2024</div>
+            <div className="timeline-end timeline-date">Q1 2024</div>
             <div className="timeline-middle">
               <Image src="/logo.png"
                 alt="corrupted pigs logo"
-                className="mb-1"
-                height={45}
-                width={45}
+                className=""
+                height={50}
+                width={50}
                 priority
               />
             </div>
@@ -26,11 +26,11 @@ const Roadmap = () => {
                 <dd>Recruit key team members, including developers, designers, and community managers.</dd>
               </dl>
             </div>
-            <hr className="bg-red-700"/>
+            <hr className="bg-red-700 glow"/>
           </li>
           <li>
-            <hr className="bg-orange-700"/>
-            <div className="timeline-start">Q2 2024</div>
+            <hr className="bg-orange-700 glow"/>
+            <div className="timeline-start timeline-date">Q2 2024</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
@@ -46,10 +46,10 @@ const Roadmap = () => {
                 <dd>Launch social media channels and engage early adopters.</dd>
               </dl>
             </div>
-            <hr className="bg-orange-600"/>
+            <hr className="bg-orange-600 glow"/>
           </li>
           <li>
-            <hr className="bg-yellow-300"/>
+            <hr className="bg-yellow-300 glow"/>
             <div className="timeline-start timeline-box bg-transparent">
               <dl>
                 <dt className="underline">NFT Collection Launch</dt>
@@ -65,12 +65,12 @@ const Roadmap = () => {
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
-            <div className="timeline-end">Q3 2024</div>
+            <div className="timeline-end timeline-date">Q3 2024</div>
             <hr/>
           </li>
           <li>
             <hr/>
-            <div className="timeline-start">Q4 2024</div>
+            <div className="timeline-start timeline-date">Q4 2024</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
@@ -87,7 +87,7 @@ const Roadmap = () => {
           </li>
           <li>
             <hr/>
-            <div className="timeline-end">Q1 2025</div>
+            <div className="timeline-end timeline-date">Q1 2025</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
@@ -107,7 +107,7 @@ const Roadmap = () => {
           </li>
           <li>
             <hr/>
-            <div className="timeline-start">Q2 2025</div>
+            <div className="timeline-start timeline-date">Q2 2025</div>
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
             </div>
