76a6aebb4918e3d058dded28800b6466f55b21cc
Nuno Costa <nuno.mmc@gmail.com>
2024-05-20 00:20:37 +0100

    fix og:image meta tag

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
   keywords: "NFT game, social good, donate to institutions, crowdfunding, animal farm, fight corruption",
   openGraph: {
     description: "Welcome to the official home of Corrupted Pigs. Join us to explore our unique NFT collection and engaging social game. Log in to access exclusive perks, participate in the burning pigs game, and discover the benefits of holding our NFTs and Coinks tokens.",
-    image: "https://corruptedpigs.com/image-twitter-CP-launching-soon.jpg"
+    image: "/image-twitter-CP-launching-soon.jpg"
   }
 };
 
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en" style={{ scrollBehavior: 'smooth' }}>
       <head>
         <link rel="shortcut icon" href="/images/favicon.ico" sizes="any"/>
+        <meta property="og:image" content={metadata.openGraph.image} />
       </head>
       <body className={inter.className}>{children}</body>
       {process.env.NODE_ENV === 'production' && (
