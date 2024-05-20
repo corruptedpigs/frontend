555cdd742a267cbe211b73e18268ff2f39af01a2
Nuno Costa <nuno.mmc@gmail.com>
2024-05-20 11:04:57 +0100

    fix missing meta tags

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
   keywords: "NFT game, social good, donate to institutions, crowdfunding, animal farm, fight corruption",
   openGraph: {
     description: "Welcome to the official home of Corrupted Pigs. Join us to explore our unique NFT collection and engaging social game. Log in to access exclusive perks, participate in the burning pigs game, and discover the benefits of holding our NFTs and Coinks tokens.",
+    logo: "https://corruptedpigs.com/images/favicon.ico",
+    type: "website",
+    url: "https://corruptedpigs.com",
     image: "/image-twitter-CP-launching-soon.jpg"
   }
 };
@@ -21,6 +24,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="shortcut icon" href="/images/favicon.ico" sizes="any"/>
         <meta property="og:image" content={metadata.openGraph.image} />
+        <meta property="og:logo" content={metadata.openGraph.logo} />
       </head>
       <body className={inter.className}>{children}</body>
       {process.env.NODE_ENV === 'production' && (
